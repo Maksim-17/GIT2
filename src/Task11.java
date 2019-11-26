1952a71051e42a3b3ea04e49ebeaef09af0f91cf
Maksim-17 <56191893+Maksim-17@users.noreply.github.com>
2019-11-26 23:36:10 +0300

    Delete Task11.java

--- src/Task11.java
+++
@@ -1,8 +0,0 @@
-/**Необходимо произвести ввод строки с клавиатуры и записать в строковую переменную. Результат вывести на экран
- * Created by ADMIN on 17.11.2019.
- */
-public class Task11 {
-    public static void main (String[]args){
-
-    }
-}
